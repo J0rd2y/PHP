2a81833397aa9fc6ac9ab41fd52f3313c6d249ca
Jordan Greenwood <41351107+J0rd2y@users.noreply.github.com>
2019-08-22 13:58:06 -0500

    Uploading Files
    Homework Page all set up

---
+++ wdv341/homeworkpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Homework Page</title>
+  </head>
+  <body>
+    <h1>PHP all of my Homework</h1>
+    
+    <h2>Unit1</h2>
+    <p><a></a></p>
+    
+    <h2>Unit2</h2>
+    <p><a></a></p>
+  </body>
+</html>
